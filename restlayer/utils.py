f9405a0a81b86ddceee9920ceb64bf5f91373b2f
Olivier Meunier <olivier@neokraft.net>
2012-11-20 22:07:21 +0100

    pylint

--- restlayer/utils.py
+++ restlayer/utils.py
@@ -12,29 +12,30 @@ from django import db
 from django.utils.encoding import smart_unicode
 from django.utils.xmlutils import SimplerXMLGenerator
 
+
 class ModelDataLoader(object):
     def __init__(self, fields):
         self.fields = fields
-    
+
     def __call__(self, res, request, **options):
         if isinstance(res, db.models.query.QuerySet):
             return [self(x, request, **options) for x in res]
-        
+
         elif isinstance(res, db.models.Model):
             return dict([
                 (x, self.get_field_value(res, x, request, **options))
                 for x in options.get('fields', ('pk',))
             ])
-        
+
         return res
-    
+
     def get_field_value(self, instance, field, request, **options):
         resp = options.get('resp')
         if resp:
             f = getattr(resp, field, None)
             if callable(f):
                 return f(instance, request)
-        
+
         try:
             f = getattr(instance, field)
             if callable(f):
@@ -42,9 +43,9 @@ class ModelDataLoader(object):
             return f
         except AttributeError:
             pass
-        
+
         raise Exception('Field %s not found.' % field)
-    
+
 
 def get_request_data(request):
     """
@@ -52,7 +53,7 @@ def get_request_data(request):
     In case we send data over PUT, Django won't
     actually look at the data and load it. We need
     to twist its arm here.
-    
+
     The try/except abominiation here is due to a bug
     in mod_python. This should fix it.
     """
@@ -65,11 +66,11 @@ def get_request_data(request):
             request.META['REQUEST_METHOD'] = 'POST'
             request._load_post_and_files()
             request.META['REQUEST_METHOD'] = 'PUT'
-            
+
         return request.POST
     elif request.method == "POST":
         return request.POST
-    
+
 
 def xml_dumps(data):
     def to_xml(xml, data):
@@ -85,16 +86,16 @@ def xml_dumps(data):
                 xml.endElement(key)
         else:
             xml.characters(smart_unicode(data))
-    
+
     stream = StringIO.StringIO()
-    
+
     xml = SimplerXMLGenerator(stream, "utf-8")
     xml.startDocument()
     xml.startElement("response", {})
-    
+
     to_xml(xml, data)
-    
+
     xml.endElement("response")
     xml.endDocument()
-    
+
     return stream.getvalue()
